0f4016ec4d79fe70d3174af37de4209cb268c195
Ryan Kirsch <ryan.kirsch@longevityconsulting.com>
2022-01-23 12:31:34 -0500

    create interface, getServerSideProps to handle SSR of posts, fetch from Sanity

--- sanity.js
+++ sanity.js
@@ -5,8 +5,16 @@ import {
 } from "next-sanity";
 
 export const config = {
+  // find info in sanity.json in studio porject
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   apiVersion: "2021-03-25",
+  //   set useCdn to false if app requires freshest data possible
   useCdn: process.env.NODE_ENV === "production",
 };
+
+// set up client for fetching data in the getProps page functions
+export const sanityClient = createClient(config);
+
+// set up helper function for generating Image URLs with only asset reference data in your documents
+export const urlFor = (source) => createImageUrlBuilder(config).image(source);
