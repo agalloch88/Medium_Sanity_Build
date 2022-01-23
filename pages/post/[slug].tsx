53e2e92d32293582ce1cc4afacd962a6547cc215
Ryan Kirsch <agalloch88@gmail.com>
2022-01-23 14:54:15 -0500

    further styling on PortableText for each post

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Header from "../../components/Header";
 import { sanityClient, urlFor } from "../../sanity";
 import { Post } from "../../typings";
+import PortableText from "react-portable-text";
 
 interface Props {
   post: Post;
@@ -29,11 +30,35 @@ function Post({ post }: Props) {
             alt="author profile picture"
           />
           <p className="font-extralight text-sm">
-            Blog post by <span className="text-green-600">{post.author.name}</span> - Published at{" "}
-            {new Date(post._createdAt).toLocaleString()}
+            Blog post by{" "}
+            <span className="text-green-600">{post.author.name}</span> -
+            Published at {new Date(post._createdAt).toLocaleString()}
           </p>
         </div>
-        
+        <div>
+          <PortableText
+            className=""
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+            content={post.body}
+            serializers={{
+              h1: (props: any) => (
+                <h1 className="text-2xl font-bold my-5" {...props} />
+              ),
+              h2: (props: any) => (
+                <h2 className="text-xl font-bold my-5" {...props} />
+              ),
+              li: ({ children }: any) => (
+                <li className="ml-4 list-disc">{children}</li>
+              ),
+              link: ({ href, children }: any) => (
+                <a href={href} className="text-blue-500 hover:underline">
+                  {children}
+                </a>
+              ),
+            }}
+          />
+        </div>
       </article>
     </main>
   );
