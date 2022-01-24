50348b1893159b38e726b4446e4bac0893ee7a59
Ryan Kirsch <agalloch88@gmail.com>
2022-01-24 08:56:32 -0500

    add Comment interface, style comment display area on post

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -95,8 +95,10 @@ function Post({ post }: Props) {
 
       {submitted ? (
         <div className="flex flex-col py-10 my-10 bg-yellow-500 text-white max-w-2xl mx-auto">
-            <h3 className="text-3xl font-bold">Thank you for submitting your comment!</h3>
-            <p>Once approved, your comment will appear below.</p>
+          <h3 className="text-3xl font-bold">
+            Thank you for submitting your comment!
+          </h3>
+          <p>Once approved, your comment will appear below.</p>
         </div>
       ) : (
         <form
@@ -162,6 +164,21 @@ function Post({ post }: Props) {
           />
         </form>
       )}
+
+      {/* comments */}
+      <div className="flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2">
+        <h3 className="text-4xl">Comments</h3>
+        <hr className="pb-2 "/>
+
+        {post.comments.map((comment) => (
+          <div key={comment._id}>
+            <p>
+              <span className="text-yellow-500">{comment.name}: </span>
+              {comment.comment}
+            </p>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
